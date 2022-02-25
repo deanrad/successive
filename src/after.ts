906b3e2c7109c990f95429a147d8283a61d0adb4
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2022-02-25 10:29:53 -0600

    0.9.10 docs

--- src/after.ts
+++ src/after.ts
@@ -1,16 +1,25 @@
 import { Observable, firstValueFrom } from 'rxjs';
 import type { Observer } from 'rxjs';
 
+/**
+ * `after` returns an Observable of the value, or result of the function call, after the number of milliseconds given.
+ * For a delay of 0, the function is executed synchronously when `.subscribe()` is called.
+ * `after` is 'thenable' - and can be awaited like a Promise.
+ * However, since underneath it is an Observable, `after` is both lazy and cancelable!
+ *
+ * @returns An Observable of the object or thunk return value, which can be the target of an `await`.
+ */
 export function after<T>(ms: number, fn: () => T) {
-  const obs = new Observable((notify:Observer<T>) => {
+  const obs = new Observable((notify: Observer<T>) => {
     if (ms === 0) {
       notify.next(fn());
-      notify.complete()
+      notify.complete();
+      return;
     }
     const id = setTimeout(() => {
       const retVal = fn();
-      notify.next(retVal)
-      notify.complete()
+      notify.next(retVal);
+      notify.complete();
     }, ms)
     return () => { id && clearTimeout(id) }
   })
