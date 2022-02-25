6dff75bf2bd873382f972ad970c319898591c0e8
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2022-02-25 00:03:07 -0600

    WIP

--- src/after.ts
+++ src/after.ts
@@ -1,6 +1,7 @@
 import { Observable, firstValueFrom } from 'rxjs';
 
 export function after<T>(ms: number, fn: () => T) {
+  // @ts-ignore
   const obs = new Observable((notify) => {
     if (ms === 0) {
       notify.next(fn());
@@ -13,11 +14,13 @@ export function after<T>(ms: number, fn: () => T) {
     }, ms)
     return () => { id && clearTimeout(id) }
   })
+
   Object.assign(obs, {
     then(resolve: (v: T) => any, reject: (e: unknown) => unknown) {
       return (firstValueFrom(obs) as PromiseLike<T>).then(resolve, reject);
     },
   });
 
+  // @ts-ignore
   return obs as Observable<T> & PromiseLike<T>;
 }
